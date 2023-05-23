60b226b462d0d13f32051c00ef38c1b196d49d37
MD NUR ISLAM <84634226+NURISLAM27878@users.noreply.github.com>
2023-05-23 18:31:49 +0600

    equal

---
+++ oparetor.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Oparetor</title>
+		
+	</head>
+	<body>
+		
+		<?php echo "oparetor"; ?>
+		
+		<?php
+		$breack = "</br>";
+			$x = 12;
+			$y = 2;
+			
+			echo $breack, $x+$y, $breack;
+			echo $x-$y, $breack;
+			echo $x*$y, $breack;
+			echo $x/$y, $breack;
+			echo $x%$y, $breack;
+			$a = 10;
+			$b = 10;
+			var_dump($a != $b);
+			
+		?>
+	</body>
+</html>
