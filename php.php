11e4515dde10dae02037ec8a56b73985e0a6aaf1
MD NUR ISLAM <84634226+NURISLAM27878@users.noreply.github.com>
2024-04-05 16:05:23 +0600

    form and array

--- php.php
+++ php.php
@@ -81,44 +81,44 @@ $a = array(
 
   $nameprint = array_column($a, 'name');
 //   print_r($nameprint);
-echo '<pre>';
-print_r($nameprint);
-echo '</pre>';
-  echo "<br>";
+// echo '<pre>';
+// print_r($nameprint);
+// echo '</pre>';
+//   echo "<br>";
 
-  $printall = array_column($a, 'name', 'id');
-//   print_r($printall);
-echo '<pre>';
-print_r($printall);
-echo '</pre>';
+//   $printall = array_column($a, 'name', 'id');
+// //   print_r($printall);
+// echo '<pre>';
+// print_r($printall);
+// echo '</pre>';
 
-  echo "<br>";  echo "<br>";  echo "<br>";
+//   echo "<br>";  echo "<br>";  echo "<br>";
 
 
 
 //   this code for homework result sheet
-$mark = [
-    "1" =>[
-        "name" => "Akash",
-        "roll" => "1",
-        "marks" => "70",
-    ],
-    "2" =>[
-        "name" => "Rajen",
-        "roll" => "2",
-        "marks" => "80",
-    ],
-    "3" =>[
-        "name" => "Nur",
-        "roll" => "3",
-        "marks" => "60",
-    ],
-];
+// $mark = [
+//     "1" =>[
+//         "name" => "Akash",
+//         "roll" => "1",
+//         "marks" => "70",
+//     ],
+//     "2" =>[
+//         "name" => "Rajen",
+//         "roll" => "2",
+//         "marks" => "80",
+//     ],
+//     "3" =>[
+//         "name" => "Nur",
+//         "roll" => "3",
+//         "marks" => "60",
+//     ],
+// ];
 
-$grate = [
-    "A+" => "80",
-    "A-" => "70",
-];
+// $grate = [
+//     "A+" => "80",
+//     "A-" => "70",
+// ];
 
 
 // if (array_key_exists("1",$mark)){
@@ -136,20 +136,71 @@ $grate = [
 // }
 
 // result();
-$roll = "2";
-if (array_key_exists($roll, $mark)) {
-    echo "Name: ". $mark[$roll]["name"] ."<br>";
-    echo "Roll: ". $mark[$roll]["roll"] ."<br>";
-    // echo "Grate: ". $mark["1"]["marks"];
-    if ($mark[$roll]["marks"] == "80" ) {
-        echo "Grate: ". "A+";
-    }if($mark[$roll]["marks"] == "70" ) {
-        echo "Grate: ". "A-";
-    }if($mark[$roll]["marks"] == "60" ) {
-        echo "Grate: ". "B";
-    }
+// $roll = "2";
+// if (array_key_exists($roll, $mark)) {
+//     echo "Name: ". $mark[$roll]["name"] ."<br>";
+//     echo "Roll: ". $mark[$roll]["roll"] ."<br>";
+//     // echo "Grate: ". $mark["1"]["marks"];
+//     if ($mark[$roll]["marks"] == "80" ) {
+//         echo "Grate: ". "A+";
+//     }if($mark[$roll]["marks"] == "70" ) {
+//         echo "Grate: ". "A-";
+//     }if($mark[$roll]["marks"] == "60" ) {
+//         echo "Grate: ". "B";
+//     }
+// }
+
+
+
+?>
+
+<form action="#" method="post" style="width:500px; margin:0 auto;">
+    <input type="text" name="fname" placeholder="First Nmae"  style="width:49%;">
+    <input type="text" name="lname" placeholder="Last Name"  style="width:49%;"><br> <br>
+    <input type="number" name="number" placeholder="Number" style="width:100%;"><br> <br>
+    <input type="text" name="subject" placeholder="Subject" style="width:100%;"><br><br>
+    <input type="mail" name="tomail" placeholder="To Email" style="width:100%;"><br><br>
+    <textarea name="massage" id="" cols="65" rows="5" placeholder="Massage"></textarea><br><br>
+    <button type="submit" name="coantact-form-submit">Submit</button><br>
+</form>
+
+<?php 
+
+if (isset($_POST['coantact-form-submit'])) {
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $number = $_POST['number'];
+    $subject = $_POST['subject'];
+    $massage = $_POST['massage'];
+    $tosent_massage = $fname . ' ' . $lname . '\n'.'Phone Number: ' . $number . ' ' . $subject . ' ' . $massage;
+    $tomail = $_POST['tomail'];
+    mail($tomail,$subject,$tosent_massage);
 }
 
+// array combine 
+
+$name = array('nur', 'rajen' , 'akash');
+$age = array('30', '40', '50');
+
+$combine = array_combine($name, $age);
+print_r($combine);
+echo "<br>";
+// count array value
+$value = ['nur', 'nur', 'rajen', 'akash','akash','akash'];
+print_r(array_count_values($value));
+echo "<br>";
+
+$value1 = array("a"=>"nur","b"=>"akash","c"=>"rajen","d"=>"hridoy");
+$value2 = array("e"=>"nur","f"=>"akash","g"=>"rajen");
+
+$valueresult = array_diff($value1,$value2);
+print_r($valueresult);
+echo "<br>";
+
+$value3 = array("a"=>"nur","b"=>"akash","c"=>"rajen","d"=>"hridoy");
+$value4 = array("a"=>"nur","f"=>"akash","g"=>"rajen");
+
+print_r(array_diff_assoc($value3,$value4));
 
 
 exit; 
